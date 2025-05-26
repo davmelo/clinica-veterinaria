06ce1ca2db6e60eec0b04e54af46f783752471fe
LucasPonte5 <133993933+LucasPonte5@users.noreply.github.com>
2025-05-26 13:00:33 -0300

    Update Vacina.java
    Adicionado nomeVacina

--- src/main/java/com/clinicaveterinaria/procedimento/model/Vacina.java
+++ src/main/java/com/clinicaveterinaria/procedimento/model/Vacina.java
@@ -6,25 +6,35 @@ import com.clinicaveterinaria.veterinario.model.Veterinario;
 import java.time.LocalDate;
 
 public class Vacina extends Procedimento{
+    private String nomeVacina;
     private int numeroDeDoses;
     private int doseNumero;
     private String tipoAplicacao;
     private LocalDate validade;
 
     public Vacina(String nome, LocalDate dataDeRealizacao, Veterinario veterinario, Animal animal,
-                  int numeroDeDoses, int doseNumero, String tipoAplicacao, LocalDate validade) {
+                  int numeroDeDoses, int doseNumero, String tipoAplicacao, LocalDate validade, String nomeVacina) {
         super(nome, dataDeRealizacao, veterinario, animal);
         this.setNome(nome);
+        this.setNomeVacina(nomeVacina);
         this.setNumeroDeDoses(numeroDeDoses);
         this.setDoseNumero(doseNumero);
         this.setTipoAplicacao(tipoAplicacao);
         this.setValidade(validade);
     }
 
+    public String getNomeVacina(){
+        return nomeVacina;
+    }
+
+    public void setNomeVacina(String nomeVacina){
+        this.nomeVacina = nomeVacina;
+    }
+    
     public int getNumeroDeDoses() {
         return numeroDeDoses;
     }
-
+    
     public void setNumeroDeDoses(int numeroDeDoses) {
         if (numeroDeDoses < 1) {
             throw new IllegalArgumentException("");
