d8e496728299986911a560c17f4dceb8fad4b65d
David Melo <dev.davmelo@gmail.com>
2025-05-26 00:01:41 -0300

    feat: Adicionar lógica da agenda para veterinários

---
+++ src/main/java/com/clinicaveterinaria/agenda/model/Agenda.java
@@ -0,0 +1,34 @@
+package com.clinicaveterinaria.agenda.model;
+
+import com.clinicaveterinaria.consulta.model.Agendamento;
+import com.clinicaveterinaria.veterinario.model.Veterinario;
+
+import java.util.ArrayList;
+
+public class Agenda {
+    private Veterinario veterinario;
+    private ArrayList<Agendamento> agendamentos;
+
+    public Agenda(Veterinario veterinario) {
+        setVeterinario(veterinario);
+        this.agendamentos = new ArrayList<>();
+    }
+
+    public Veterinario getVeterinario() {
+        return veterinario;
+    }
+
+    public void setVeterinario(Veterinario veterinario) {
+        if (veterinario == null) {
+            throw new IllegalArgumentException("");
+        }
+
+        this.veterinario = veterinario;
+    }
+
+    public boolean horarioDisponivel() {
+        return false;
+    }
+
+    public void adiconarAgendamento() {}
+}
